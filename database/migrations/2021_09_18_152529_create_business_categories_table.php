0346551c86c66b4d865b40fde796753c3ffa0426
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-09-18 16:52:05 +0700

    Add BusinessCategory Migration and Model

---
+++ database/migrations/2021_09_18_152529_create_business_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusinessCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('business_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('initial', 10)->nullable()->default(NULL);
+            $table->string('slug');
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('business_categories');
+    }
+}
