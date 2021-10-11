fa83ce1a794b2c57630a44450c33f91eed49e3d0
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-10-11 15:00:52 +0700

    Add UserBankAccount Model and Migration

---
+++ database/migrations/2021_10_11_144813_create_user_bank_accounts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserBankAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_bank_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('bank_id');
+            $table->string('branch');
+            $table->string('account_number');
+            $table->string('alias_name');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('bank_id')->references('id')->on('banks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_bank_accounts');
+    }
+}
