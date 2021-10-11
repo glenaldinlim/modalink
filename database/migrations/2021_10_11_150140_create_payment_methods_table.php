68d529c35bfb40d7a7c8ba2ce81eae4bba1d2f1b
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-10-11 18:35:30 +0700

    Add PaymentMethod Model and Migration

---
+++ database/migrations/2021_10_11_150140_create_payment_methods_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentMethodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_methods', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->comment('PIC of Payment');
+            $table->foreignId('bank_id');
+            $table->string('account_number');
+            $table->string('alias_name');
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('bank_id')->references('id')->on('banks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_methods');
+    }
+}
