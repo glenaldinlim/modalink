4eb8163da58c9cc9313bb269bf095d108614a58a
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-10-18 11:37:31 +0700

    Edit Fund and FundDetail Models

--- app/Models/Fund.php
+++ app/Models/Fund.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Fund extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'merchant_id',
+        'fund_type_id',
+        'name',
+        'description',
+        'tenor',
+        'interest_rate',
+        'fund_target',
+        'deadline',
+        'price_per_unit',
+        'fund_status_id',
+    ];
 }
--- app/Models/FundDetail.php
+++ app/Models/FundDetail.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class FundDetail extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'fund_id',
+        'user_id',
+        'payment_method_id',
+        'unit',
+        'purchase_status',
+        'verification_by',
+    ];
 }
