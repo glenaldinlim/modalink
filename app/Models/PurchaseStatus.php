2232188851304a0a95215e58de50a550cb2418c7
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-10-18 18:19:35 +0700

    Edit PurchaseStatus Model

--- app/Models/PurchaseStatus.php
+++ app/Models/PurchaseStatus.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class PurchaseStatus extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'description',
+    ];
 }
